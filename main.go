af16004fbacc5f0104a10d360e2c43e622bb4f3a
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2020-06-20 13:55:31 -0500

    Fix environment variables load

--- main.go
+++ main.go
@@ -18,44 +18,40 @@ func getServers(ctx *fasthttp.RequestCtx) {
 }
 
 func main() {
-	err := goDotenv.Load()
+	goDotenv.Load()
+	//F Init environment variables...
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	port := os.Getenv("PORT")
+
+	// Init database...
+	db, err := db.StartPostgresqlConnection(dbUser, dbHost, dbName)
 	if err != nil {
-		log.Fatal("Environmnet variables cannot be loaded")
+		log.Fatal("Database cannot be started")
 	} else {
-		// Init environment variables...
-		dbHost := os.Getenv("DB_HOST")
-		dbName := os.Getenv("DB_NAME")
-		dbUser := os.Getenv("DB_USER")
-		port := os.Getenv("PORT")
+		// Init repositories...
+		domainRepo := repositories.NewDomainRepository(db)
+		domainService := services.NewDomainService()
+		domainHandler := controllers.NewDomainHandler(domainRepo, domainService)
 
-		// Init database...
-		db, err := db.StartPostgresqlConnection(dbUser, dbHost, dbName)
-		if err != nil {
-			log.Fatal("Database cannot be started")
-		} else {
-			// Init repositories...
-			domainRepo := repositories.NewDomainRepository(db)
-			domainService := services.NewDomainService()
-			domainHandler := controllers.NewDomainHandler(domainRepo, domainService)
+		// Init router...
+		router := fasthttprouter.New()
+		router.GET("/api/v1/analyze", domainHandler.CheckDomain)
 
-			// Init router...
-			router := fasthttprouter.New()
-			router.GET("/api/v1/analyze", domainHandler.CheckDomain)
-
-			withCors := cors.NewCorsHandler(cors.Options{
-				// if you leave allowedOrigins empty then fasthttpcors will treat it as "*"
-				AllowedOrigins: []string{"*"}, // Only allow example.com to access the resource
-				// if you leave allowedHeaders empty then fasthttpcors will accept any non-simple headers
-				AllowedHeaders: []string{"x-something-client", "Content-Type"}, // only allow x-something-client and Content-Type in actual request
-				// if you leave this empty, only simple method will be accepted
-				AllowedMethods:   []string{"GET", "POST"}, // only allow get or post to resource
-				AllowCredentials: false,                   // resource doesn't support credentials
-				AllowMaxAge:      5600,                    // cache the preflight result
-				Debug:            true,
-			})
-			if err := fasthttp.ListenAndServe(":"+port, withCors.CorsMiddleware(router.Handler)); err != nil {
-				log.Fatalf("Error in ListenAndServe: %s", err.Error())
-			}
+		withCors := cors.NewCorsHandler(cors.Options{
+			// if you leave allowedOrigins empty then fasthttpcors will treat it as "*"
+			AllowedOrigins: []string{"*"}, // Only allow example.com to access the resource
+			// if you leave allowedHeaders empty then fasthttpcors will accept any non-simple headers
+			AllowedHeaders: []string{"x-something-client", "Content-Type"}, // only allow x-something-client and Content-Type in actual request
+			// if you leave this empty, only simple method will be accepted
+			AllowedMethods:   []string{"GET", "POST"}, // only allow get or post to resource
+			AllowCredentials: false,                   // resource doesn't support credentials
+			AllowMaxAge:      5600,                    // cache the preflight result
+			Debug:            true,
+		})
+		if err := fasthttp.ListenAndServe(":"+port, withCors.CorsMiddleware(router.Handler)); err != nil {
+			log.Fatalf("Error in ListenAndServe: %s", err.Error())
 		}
 	}
 }
